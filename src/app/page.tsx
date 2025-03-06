a2f2fe97bd3d826812ee4f398ee4b082b3854501
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-03-06 17:17:25 -0300

    ♻️ refactor: adiciona tipagem explícita à função getAllPosts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { CardPost } from '@/components/CardPost'
+import { Post } from '@/types/Post'
 
-async function getAllPosts() {
+async function getAllPosts(): Promise<Post[]> {
     const response = await fetch('http://localhost:3042/posts')
 
     if (!response.ok) {
