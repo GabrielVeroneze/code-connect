ee05f47b9007b73e8b88f778f663d32d46781013
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-03-09 17:00:48 -0300

    ✨ feat: processa conteúdo Markdown para HTML na página de post

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
-import logger from '@/logger'
+import { remark } from 'remark'
 import { Post } from '@/types/Post'
+import html from 'remark-html'
+import logger from '@/logger'
 
 async function getPostBySlug(slug: string): Promise<Post | null> {
     const response = await fetch(`http://localhost:3042/posts?slug=${slug}`)
@@ -18,7 +20,14 @@ async function getPostBySlug(slug: string): Promise<Post | null> {
         return null
     }
 
-    return data[0]
+    const post = data[0]
+
+    const processedContent = await remark().use(html).process(post.markdown)
+    const contentHtml = processedContent.toString()
+
+    post.markdown = contentHtml
+
+    return post
 }
 
 const PagePost = async ({ params }: { params: Promise<{ slug: string }> }) => {
@@ -27,8 +36,11 @@ const PagePost = async ({ params }: { params: Promise<{ slug: string }> }) => {
     const post = await getPostBySlug(slug)
 
     return (
-		<h1>{post?.title}</h1>
-	)
+        <div>
+            <h1>{post?.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+        </div>
+    )
 }
 
 export default PagePost
