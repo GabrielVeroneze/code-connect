8fc6b7c59cefdfaa9bd1680d7e8b7feef31b9725
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-03-16 20:58:05 -0300

    ♻️ refactor: extrai a função getPostBySlug para postService

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -12,33 +12,6 @@ const roboto_mono = Roboto_Mono({
     display: 'swap',
 })
 
-async function getPostBySlug(slug: string): Promise<Post | null> {
-    const response = await fetch(`http://localhost:3042/posts?slug=${slug}`)
-
-    if (!response.ok) {
-        logger.error('Ops, alguma coisa deu errado')
-
-        return null
-    }
-
-    logger.info('Post obtido com sucesso')
-
-    const data: Post[] = await response.json()
-
-    if (data.length === 0) {
-        return null
-    }
-
-    const post = data[0]
-
-    const processedContent = await remark().use(html).process(post.markdown)
-    const contentHtml = processedContent.toString()
-
-    post.markdown = contentHtml
-
-    return post
-}
-
 const PagePost = async ({ params }: { params: Promise<{ slug: string }> }) => {
     const { slug } = await params
 
